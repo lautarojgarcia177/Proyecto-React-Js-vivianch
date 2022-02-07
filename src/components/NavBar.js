1312706222ea89f931b5eaa69fa82479802e059f
Andrea Eurrieta <deyeurrieta@gmail.com>
2022-02-07 20:04:28 -0300

    navbar commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,38 +2,17 @@ import react from 'react';
 
 const Navbar = () =>{
     return(
-        <Navbar bg="light" expand={false}>
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-          <Navbar.Offcanvas
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Inicio</Nav.Link>
-                <Nav.Link href="#action2">Productos</Nav.Link>
-                <Nav.Link href="#action3">Sale</Nav.Link>
-              </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
+       <nav>
+           <div>
+            <img src="" />
+           </div>
+           <div>
+             <a>Inicio</a>
+             <a>Productos</a>
+             <a>Contacto</a>
+             <a>Sale</a>
+           </div>
+        </nav> 
     )
 }
-
 export default Navbar;
