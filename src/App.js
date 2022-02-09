ce5b707312d5df2165e3299628b9a93dbaac68b5
Andrea Eurrieta <deyeurrieta@gmail.com>
2022-02-08 21:25:03 -0300

    navbar commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Navbar} from './components/NavBar';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,33 @@ import react from 'react';
 
 const Navbar = () =>{
     return(
-       <nav>
-           <div>
-            <img src="" />
-           </div>
-           <div>
-             <a>Inicio</a>
-             <a>Productos</a>
-             <a>Contacto</a>
-             <a>Sale</a>
-           </div>
-        </nav> 
+      <nav className="navbar navbar-dark bg-dark fixed-top">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">VIVIANCH</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">VIVIANCH</h5>
+        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body bg-dark">
+        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contácto</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Sale</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
     )
 }
 export default Navbar;
