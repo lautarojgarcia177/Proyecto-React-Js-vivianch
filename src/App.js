239a1811b432c410802ae2115eb93a090b09ac16
Andrea Eurrieta <deyeurrieta@gmail.com>
2022-02-17 00:30:59 -0300

    contador commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import React from 'react';
 import {NavBar} from './components/NavBar';
 import {ItemListContainer} from './components/ItemListContainer';
+import {ItemCount} from './components/ItemCount'
+
+
+const onAdd= (count)=>{
+  alert(`Se agregaron ${count} productos al carrito`);
+}
 
 function App() {
   return (
@@ -11,6 +17,7 @@ function App() {
       <header className="App-header">
      <NavBar/>
      <ItemListContainer greeting="HOLAAAA TUTOR"/>
+     <ItemCount initial={1} stock={8} onAdd={onAdd}/>
      </header>
     </div>
     </React.Fragment>
---
+++ src/components/ItemCount.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+export const ItemCount=({initial, stock, onAdd})=>{
+      const [count, setCount] = useState(initial);
+
+        const handleAdd=()=>{
+        if(count < stock){
+        setCount(count+1);
+        }
+    }
+    const handleRemove=()=>{
+        if(count > initial){
+        setCount(count-1);
+        }
+    }
+   
+    return(
+<div className="btn-group" role="group" aria-label="Basic outlined example">
+  <button onClick={handleAdd} type="button" className="btn btn-outline-primary">+</button>
+  <imput type="number" className="form-control">{count}</imput>
+  <button onClick={handleRemove} type="button" className="btn btn-outline-primary">-</button>
+<button onClick={onAdd(count)} type="button" className="btn btn-primary">Agregar al carrito</button>
+</div>
+    )
+}
